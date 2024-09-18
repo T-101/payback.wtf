69d59b14cd82947c3337efd5be76469bffecf280
T-101 <antti.rummukainen@gmail.com>
2024-09-18 14:53:08 +0300

    added autocomplete to questionnaire visitor. changed tuples to lists

--- payback/admin.py
+++ payback/admin.py
@@ -5,36 +5,23 @@ from .models import PaybackUser, Questionnaire, Settings
 
 @admin.register(PaybackUser)
 class PaybackUserAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'created',
-        'modified',
-        'handle',
-        'group',
-        'email',
-        'user_id',
-        'name_visible',
-        'visitor_accepted',
-        'payment_status',
-    )
-    list_filter = (
-        'created',
-        'modified',
-        'name_visible',
-        'visitor_accepted',
-        'payment_status',
-    )
-    search_fields = ('handle', 'group', 'email')
+    list_display = [
+        'id', 'handle', 'group', 'email',
+        'name_visible', 'visitor_accepted', 'payment_status', 'created'
+    ]
+    list_filter = ['created', 'modified', 'name_visible', 'visitor_accepted', 'payment_status']
+    search_fields = ['handle', 'group', 'email', 'user_id']
     readonly_fields = ['user_id']
 
 
 @admin.register(Questionnaire)
 class QuestionnaireAdmin(admin.ModelAdmin):
-    list_display = ('id', 'created', 'modified', 'user', 'wants_food')
-    list_filter = ('created', 'modified', 'user', 'wants_food')
+    list_display = ['id', 'created', 'modified', 'user', 'wants_food']
+    list_filter = ['created', 'modified', 'user', 'wants_food']
+    autocomplete_fields = ['user']
 
 
 @admin.register(Settings)
 class SettingsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'registration_start')
-    list_filter = ('registration_start',)
+    list_display = ['id', 'registration_start']
+    list_filter = ['registration_start']
