302081dce3ec13ca96d154100baf11d15ec3bdf6
T-101 <antti.rummukainen@gmail.com>
2024-09-20 12:27:33 +0300

    payment urls

--- payback/urls.py
+++ payback/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from payback.payment_views import create_checkout_session
 from payback.views import LandingPageView, RegistrationView, VisitorListView, InformationView, VisitorDetailView
+from payback.payment_views import stripe_success, stripe_cancel
 
 app_name = 'payback'
 
@@ -10,4 +12,9 @@ urlpatterns = [
     path('visitors/', VisitorListView.as_view(), name='visitors'),
     path('information/', InformationView.as_view(), name='information'),
     path('visitor/<str:user_id>/', VisitorDetailView.as_view(), name='visitor-detail'),
+    # Stripe urls
+    path('stripe/create-checkout-session/', create_checkout_session, name='stripe-create-checkout-session'),
+    path('stripe/success.html', stripe_success, name='stripe-success'),
+    path('stripe/cancel.html', stripe_cancel, name='stripe-cancel'),
+
 ]
