cbad58e2829c682f8927ccd24e139f7d6dda9440
T-101 <antti.rummukainen@gmail.com>
2024-09-23 15:13:38 +0300

    added signal to send email to new user and/or when email changes

--- payback/models.py
+++ payback/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.utils import timezone
 from django_extensions.db.fields import RandomCharField
 from django_extensions.db.models import TimeStampedModel
 from singleton_models.models import SingletonModel
+
+from payback.helpers import send_registration_email
 
 
 class PaybackUser(TimeStampedModel):
@@ -45,3 +49,14 @@ class Settings(SingletonModel):
 
     def __str__(self):
         return "Settings"
+
+
+@receiver(pre_save, sender=PaybackUser)
+def send_email(sender, instance, **kwargs):
+    try:
+        old_instance = PaybackUser.objects.get(pk=instance.pk)
+    except PaybackUser.DoesNotExist:
+        return send_registration_email(instance)
+
+    if old_instance.email != instance.email:
+        return send_registration_email(instance)
