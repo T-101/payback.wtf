d054bf9f4f11ee5923216391b40222eace130603
T-101 <antti.rummukainen@gmail.com>
2024-09-23 15:13:53 +0300

    added email stuff

--- payback/helpers.py
+++ payback/helpers.py
@@ -1,11 +1,46 @@
-import sendgrid
+import os
+import threading
 
+from django.contrib.sites.models import Site
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from django.conf import settings
 
 
-def sendgrid_email(to, subject, content):
-    sg = sendgrid.SendGridAPIClient(settings.SENDGRID_API_KEY)
-    from_email = sendgrid.Email(settings.SENDGRID_FROM_EMAIL)
-    mail = sendgrid.Mail(from_email, sendgrid.Email(to), subject, sendgrid.Content("text/plain", content))
-    response = sg.client.mail.send.post(request_body=mail.get())
-    return response
+class EmailThread(threading.Thread):
+    def __init__(self, subject: str, text_filepath: str, context: dict, recipient_list: list):
+        self.subject = subject
+        self.recipient_list = recipient_list
+        self.text_filepath = text_filepath
+        self.context = context
+        threading.Thread.__init__(self)
+
+    def run(self):
+        text_content = render_to_string(self.text_filepath, context=self.context)
+
+        msg = EmailMultiAlternatives(
+            subject=self.subject,
+            body=text_content,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=self.recipient_list
+        )
+        html_filepath = self.text_filepath.replace(".txt", ".html")
+        if os.path.exists(os.path.join(settings.BASE_DIR, "payback", "templates", html_filepath)):
+            html_content = render_to_string(html_filepath, context=self.context)
+            msg.attach_alternative(html_content, "text/html")
+        msg.send()
+
+
+def send_async_mail(**kwargs):
+    EmailThread(**kwargs).start()
+
+
+def send_registration_email(payback_user):
+    send_async_mail(
+        subject="Welcome to Payback!",
+        text_filepath="snippets/email-registration.txt",
+        context={
+            "site": Site.objects.get_current().domain,
+            "handle": payback_user.handle,
+            "user_id": payback_user.user_id},
+        recipient_list=[payback_user.email])
