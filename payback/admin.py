77c24e6acc51b13bfb6f735fcc5b49bc2d71ab95
T-101 <antti.rummukainen@gmail.com>
2024-09-23 16:41:45 +0300

    actions to send registration and declined mails

--- payback/admin.py
+++ payback/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import PaybackUser, Questionnaire, Settings
+from .helpers import send_registration_email, send_declined_email
 
 
 class InlineQuestionnaire(admin.TabularInline):
@@ -18,6 +19,23 @@ class PaybackUserAdmin(admin.ModelAdmin):
     search_fields = ['handle', 'group', 'email', 'user_id']
     readonly_fields = ['user_id']
     inlines = [InlineQuestionnaire]
+
+    actions = ['send_registration_email', 'send_declined_email']
+
+    def send_registration_email(self, request, queryset):
+        for user in queryset:
+            send_registration_email(user)
+        self.message_user(request, f"{len(queryset)} Registration emails sent.")
+
+    send_registration_email.short_description = "Send registration email"
+
+    def send_declined_email(self, request, queryset):
+        queryset.update(visitor_accepted=False)
+        for user in queryset:
+            send_declined_email(user)
+        self.message_user(request, f"{len(queryset)} Declined emails sent.")
+
+    send_declined_email.short_description = "Decline user and send email"
 
 
 @admin.register(Questionnaire)
