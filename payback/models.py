9b5f47ef23e790f5fc5ef253f84b1151bd55bc9f
T-101 <antti.rummukainen@gmail.com>
2024-10-15 15:19:19 +0300

    added place_in_line

--- payback/models.py
+++ payback/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.utils import timezone
 from django_extensions.db.fields import RandomCharField
@@ -15,12 +15,17 @@ class PaybackUser(TimeStampedModel):
     group = models.CharField(max_length=100, blank=True, null=True)
     email = models.EmailField(unique=True)
     user_id = RandomCharField(length=32, unique=True, lowercase=True, include_digits=True)
+    place_in_line = models.IntegerField(default=0)
 
     initial_email_sent = models.BooleanField(default=False)
 
     name_visible = models.BooleanField(default=True)
     visitor_accepted = models.BooleanField(default=True)
     payment_status = models.BooleanField(default=False)
+
+    def regenerate_user_id(self):
+        self.user_id = ""
+        self.save()
 
     def __str__(self):
         return self.handle
@@ -53,16 +58,26 @@ class Settings(SingletonModel):
 
 
 @receiver(pre_save, sender=PaybackUser)
-def send_email(sender, instance, **kwargs):
+def send_email_if_email_changed(sender, instance, **kwargs):
+    print("INSTANCE", instance)
+
+    if instance.place_in_line == 0:
+        last_position = PaybackUser.objects.order_by("place_in_line").last().place_in_line
+        instance.place_in_line = last_position + 1
+
     if not settings.SEND_EMAILS:
         return
+
     try:
         old_instance = PaybackUser.objects.get(pk=instance.pk)
     except PaybackUser.DoesNotExist:
-        if PaybackUser.objects.count() <= 100:
-            return send_registration_email(instance)
-        else:
-            return
+        return
 
     if old_instance.email != instance.email:
+        return send_registration_email(instance)
+
+
+@receiver(post_save, sender=PaybackUser)
+def send_initial_email(sender, instance, created, **kwargs):
+    if created and settings.SEND_EMAILS and instance.place_in_line <= 100:
         return send_registration_email(instance)
